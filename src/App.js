205effe2bf9c57a513c96b5679ccd34de691f7cd
Pankaj Dekate <pankaj.dekate@workday.com>
2020-03-14 19:15:28 -0700

    reverted to default

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome to React
+          Learn React
         </a>
       </header>
     </div>
